5eef5bf288ad2528f011d76201a4f20e73f60d43
Nikita Korolev <nikitakorolov6@gmail.com>
2021-05-04 14:38:55 +0600

    learn basic properties of some widget

--- first_lesson/myfirstapp/lib/main.dart
+++ first_lesson/myfirstapp/lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
     appBar:  AppBar(
       title: Text("My First App"),
       centerTitle: true,
+      backgroundColor: Colors.green[200],
     ),
     body: Center(
-      child: Text("Hello Nikita!"),
+      child: Text(
+        "Hello Nikita!",
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: "IndieFlower"
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: () {},
       child: Text("Click"),
+      backgroundColor: Colors.green[200],
     ),
   ),
 ));
