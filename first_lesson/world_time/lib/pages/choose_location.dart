ddbcb209dd3fcc5e63156b6c4467d59aca04c987
Nikita Korolev <nikitakorolov6@gmail.com>
2021-05-06 19:24:53 +0600

    learn http package from pub.dev

--- first_lesson/world_time/lib/pages/choose_location.dart
+++ first_lesson/world_time/lib/pages/choose_location.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({Key key}) : super(key: key);
@@ -9,26 +8,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    // simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'nikita';
-    });
-
-    // simulate network request to get bio of the username
-    String bio = await Future.delayed(Duration(seconds: 3), () {
-      return "simple example";
-    });
-
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
   @override
   Widget build(BuildContext context) {
     print('build function run');
--- first_lesson/world_time/lib/pages/loading.dart
+++ first_lesson/world_time/lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({Key key}) : super(key: key);
@@ -8,6 +10,27 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getTime() async {
+    // make the request
+    Uri timeUri = Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Omsk');
+    Response response = await get(timeUri);
+    Map data = jsonDecode(response.body);
+
+    // get property from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
